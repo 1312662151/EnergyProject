3fea0aae6376af0d7407f66086aeb3ddf752a259
LiuJie <1312662151@qq.com>
2018-01-11 12:56:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/viewpagertest/ViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.viewpagertest;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by 杰 on 2017/12/27.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private List<Fragment> fragments;
+    private String[] titles;
+
+    public ViewPagerAdapter(FragmentManager fm, String[] titles, List<Fragment> fragments) {
+        super(fm);
+        this.titles=titles;
+        this.fragments=fragments;
+    }
+
+    @Override
+    public Fragment getItem(int arg0) {
+        return fragments.get(arg0);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+}
